b9c2d22a908431c48a1d8c108fea8b40186146ce
reymond <josephreymond2011@gmail.com>
2019-11-04 15:59:44 +0530

    commit10 reymond

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('tasks',broker='amqp://ec2-52-66-180-183.ap-south-1.compute.amazonaws.com//')
+app = Celery('tasks',broker='amqp://locahost//')
 
 @app.task
 def reverse(string):
